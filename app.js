968607b0562084069e0cd714495361447759e991
qianqian <18301136199@163.com>
2021-09-14 18:30:13 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const ejsMate = require("ejs-mate");
+
+const app = express();
+
+//layout for every page
+app.engine('ejs',ejsMate);
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(__dirname + '/views'));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/',(req,res) => {
+    res.render('home');
+});
+
+app.get('/login', (req,res) => {
+    res.render('login');
+});
+
+app.get('/sign-up', (req, res) => {
+    res.render('sign-up');
+})
+
+// app.post('/campgrounds', async(req, res) => {
+//     const campground = new Campground(req.body.campground);
+//     await campground.save();
+//     res.redirect(`/campgrounds/${campground._id}`);
+// })
+
+// app.get('/campgrounds/:id', async(req, res) => {
+//     const campground = await Campground.findById(req.params.id);
+//     res.render('campgrounds/show', {campground});
+// });
+
+// app.get('/campgrounds/:id/edit', async(req, res) => {
+//     const campground = await Campground.findById(req.params.id);
+//     res.render('campgrounds/edit', {campground});
+// });
+// app.put('/campgrounds/:id', async (req, res) => {
+//     const {id} = req.params;
+//     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+//     res.redirect(`/campgrounds/${campground._id}`);
+// });
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+});
