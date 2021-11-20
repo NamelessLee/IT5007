9b644b02d43cafa2fd3501c5e2ba29334500f5da
Xinyue <296725320@qq.com>
2021-11-20 20:53:58 +0800

    Fixed sessionStorage naming

--- views/scripts/login.js
+++ views/scripts/login.js
@@ -12,7 +12,8 @@ async function handleLoginSubmit(){
     console.log("userInDb="+JSON.stringify(userInDb));
     var pwd = userInDb.password;
     if(pwd == user.password){//successful on login
-        sessionStorage.setItem('username',userInDb.name);  //当前登录用户存进session      
+        sessionStorage.setItem('name',userInDb.name);  //当前登录用户存进session    
+        sessionStorage.setItem('username',userInDb.username);
         alert("successful on login");//提示成功
         window.location.href='/'; //跳转回主页
         document.getElementById("currentUser").innerText = userInDb.name;//主页currentUser显示登入用户的名字
