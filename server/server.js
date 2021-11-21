6a0e34e6327dae1f12a4a525d8f83e01e1211219
Xinyue <296725320@qq.com>
2021-11-21 20:20:35 +0800

    Some bugs in building-management.ejs

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ let aboutMessage = "Safe Management";
 
 const resolvers = {
   Query: {
+    Building,
     Positive,
     about: () => aboutMessage,
     User,
@@ -29,6 +30,12 @@ const resolvers = {
     HealthAdd,
   }
 };
+
+async function Building(_, {date}) {
+  const buildings = await db.collection('buildings').find({Date: date}).toArray();
+  //console.log(date + " : positives="+ JSON.stringify(positives));
+  return buildings;
+}
 
 async function Positive(_, {date}) {
   const positives = await db.collection('positive').find({Date: date}).toArray();
@@ -96,7 +103,7 @@ app.all("*",function(req,res,next){
   res.header("Access-Control-Allow-Headers","content-type");
   res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
   if (req.method.toLowerCase() == 'options')
-      res.send(200);
+      res.sendStatus(200);
   else
       next();
 });
